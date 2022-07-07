745524dc92382e9d444f6df6201d94828635ffa5
Kavya Jalan <77575481+Kavyaj21@users.noreply.github.com>
2022-07-08 00:11:17 +0530

    Update metacoinloan_template.sol

--- Assignment 2/metacoinloan_template.sol
+++ Assignment 2/metacoinloan_template.sol
@@ -36,21 +36,32 @@ contract Loan is MetaCoin {
     mapping (address => uint256) private loans;
      
     event Request(address indexed _from, uint256 P, uint R, uint T, uint256 amt);
-    
-    address private Owner;
 
+    address private Owner;
     
     modifier isOwner() {
-        // Implement a modifier to allow only the owner of the contract to use specific functions
+        require(msg.sender == Owner);
+        _;
     }
     
     constructor() public {
-        // Make the creator of the contract the Owner.
+        Owner = msg.sender;
         // You can take the help of 2_owner.sol contract in remix for this and the above function. 
     }
     
     // Fill up the following function definitions and also try to justify why some functions are pure and some are view and some are none, in your README.md
-    
+    function pow (uint x, uint n) public pure returns (uint r) {
+        r = 1.0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+            r *= x;
+            n -= 1;
+            } else {
+            x *= x;
+            n /= 2;
+            }
+        }
+    }
     function getCompoundInterest(uint256 principle, uint rate, uint time) public pure returns(uint256) {
     		// Anyone should be able to use this function to calculate the amount of Compound interest for given P, R, T
         // Solidity does not have a good support for fixed point numbers so we input the rate as a uint
@@ -58,12 +69,22 @@ contract Loan is MetaCoin {
         // https://medium.com/coinmonks/math-in-solidity-part-4-compound-interest-512d9e13041b just read the periodic compounding part and
         // https://medium.com/coinmonks/math-in-solidity-part-3-percents-and-proportions-4db014e080b1 just read the towards full proportion part.
         // A good way to prevent overflows will be to typecast principle, rate and the big number divider suggested in the above blogs as uint256 variables, just use uint256 R = rate;
-        
+        return principle * pow (1 + rate/100, time);
     }
     
     function reqLoan(uint256 principle, uint rate, uint time) public returns(bool correct) {
         uint256 toPay = getCompoundInterest(principle, rate, time);
-        // A creditor uses this function to request the Owner to settle his loan, and the amount to settle is calculated using the inputs.
+        address cred=msg.sender;
+        loans[cred]= toPay;
+        if(loans[cred]!=0){
+            emit Request(Owner,principle,rate,time,toPay);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        // A cred uses this function to request the Owner to settle his loan, and the amount to settle is calculated using the inputs.
         // Add appropriate definition below to store the loan request of a contract in the loans mapping,
         // Also emit the Request event after succesfully adding to the mapping, and return true. Return false if adding to the mapping failed (maybe the user entered a float rate, there were overflows and toPay comes to be lesser than principle, etc.
     }
@@ -71,9 +92,26 @@ contract Loan is MetaCoin {
     function getOwnerBalance() public view returns(uint256) {
 				// use the getBalance function of MetaCoin contract to view the Balance of the contract Owner.
 				// hint: how do you access the functions / variables of the parent class in your favorite programming language? It is similar to that in solidity as well!
-		}
+            return getBalance(Owner);
+        }
     
+    function viewDues(address cred)public view isOwner returns(uint256){
+    //require(msg.sender == Owner, "This can only be called by the contract owner!");
+    return loans[cred];
+    }
     // implement viewDues and settleDues which allow *ONLY* the owner to *view* and *settle* his loans respectively. They take in the address of a creditor as arguments. viewDues returns a uint256 corresponding to the due amount, and does not modify any state variables. settleDues returns a bool, true if the dues were settled and false otherwise. Remember to set the the pending loan to 0 after settling the dues.
     // use sendCoin function of MetaCoin contract to send the coins required for settling the dues.
     
+    function settleDues(address cred) public isOwner returns (bool){
+        sendCoin(Owner, loans[cred], cred);
+        if(sendCoin(Owner, loans[cred], cred)){
+            loans[cred] = 0;
+        }
+        if(loans[cred] == 0){
+            return true;
+        }
+        else{
+        return false;
+        }
+    }
 }
